3ae7685f3d5e64f7cd2d3b273b400b89212dc316
Matthiosso <matthieuclement2012@gmail.com>
2023-02-22 07:45:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# vannilajs-d3
+
+## Description
+Interface web simplissime de CSV vers graphique
+
+## Usage
+Ouvrir le fichier `index.html` dans un navigateur.
---
+++ index.js
@@ -0,0 +1,45 @@
+function readFile(file) {
+    console.log('file', file);
+    const csvFile = file;
+
+    var reader = new FileReader();
+    reader.readAsText(csvFile, "UTF-8");
+    
+    
+    reader.onload = ((event) => {
+      var rows  = d3.csvParseRows(event.target.result),
+        table = d3.select('body').append('table')
+                  .style("border-collapse", "collapse")
+                  .style("border", "2px black solid");
+    
+      // headers
+      table.append("thead").append("tr")
+        .selectAll("th")
+        .data(rows[0])
+        .enter().append("th")
+        .text(function(d) { return d; })
+        .style("border", "1px black solid")
+        .style("padding", "5px")
+        .style("background-color", "lightgray")
+        .style("font-weight", "bold")
+        .style("text-transform", "uppercase");
+    
+      // data
+      table.append("tbody")
+        .selectAll("tr").data(rows.slice(1))
+        .enter().append("tr")
+        .selectAll("td")
+        .data(function(d){return d;})
+        .enter().append("td")
+        .style("border", "1px black solid")
+        .style("padding", "5px")
+        .on("mouseover", function(){
+        d3.select(this).style("background-color", "powderblue");
+      })
+        .on("mouseout", function(){
+        d3.select(this).style("background-color", "white");
+      })
+        .text(function(d){return d;})
+        .style("font-size", "12px");
+    })
+}
